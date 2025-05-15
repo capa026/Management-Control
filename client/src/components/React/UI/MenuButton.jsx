da1e541a5f2d5feb0924a1b8765918e95c49ac94
capa026 <carlospacheco.fx@gmail.com>
2025-05-15 13:18:12 -0400

    UPDATE Toggle sidebar Button, add btn animation

--- client/src/components/React/UI/MenuButton.jsx
+++ client/src/components/React/UI/MenuButton.jsx
@@ -1,16 +1,21 @@
 import { Menu } from "lucide-react";
 import Sidebar from "../Sidebar";
 import { useState } from "react";
+import { Icon } from "@iconify-icon/react";
 
 const MenuButton = ({ currentPage }) => {
   const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <>
       <div
-        className="md:hidden absolute top-0 right-0 h-full border-l aspect-square border-gray-800 flex select-none cursor-pointer hover:bg-gray-800 hover:text-teal-400 active:bg-gray-800 active:text-teal-400"
+        className="md:hidden flex items-center justify-center absolute top-0 right-0 h-full border-l aspect-square border-gray-800 select-none cursor-pointer hover:bg-gray-800 hover:text-teal-400 active:bg-gray-800 active:text-teal-400"
         onClick={() => setOpenSidebar(!openSidebar)}
       >
-        <Menu className="m-auto" />
+        {openSidebar ? (
+          <Icon icon="line-md:close-small" width={28} />
+        ) : (
+          <Icon icon="line-md:close-to-menu-alt-transition" width={28} />
+        )}
       </div>
 
       <Sidebar openSidebar={openSidebar} currentPage={currentPage} />
