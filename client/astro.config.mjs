debc9baa578ba361cc2039ba8d58c29f97ad5a43
capa026 <carlospacheco.fx@gmail.com>
2025-05-20 09:31:36 -0400

    DELETE

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -5,12 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react(), icon()],
+  adapter: netlify(),
 });
