b36ac2d286ea2e7aa33dcd4b91764e8fe62500cd
capa026 <carlospacheco.fx@gmail.com>
2025-05-26 08:11:21 -0400

    UPDATE: click each row to show the complete invoice

--- client/src/components/React/Invoices/UI/InvoicesTable.jsx
+++ client/src/components/React/Invoices/UI/InvoicesTable.jsx
@@ -1,4 +1,8 @@
 import { Icon } from "@iconify-icon/react";
+import Modal from "../../UI/Modal";
+import ReactQueryProvider from "../../ReactQueryProvider";
+import { useState } from "react";
+import InvoiceEdit from "./InvoiceEdit";
 
 const PAYMENT_STATUSES = {
   UNPAID: { style: "text-yellow-900 bg-yellow-100", text: "CxC" },
@@ -22,39 +26,62 @@ const ThWithIcon = ({ icon, title, onClick }) => {
 };
 
 const InvoicesTable = ({ data }) => {
+  const [selectedElement, setSelectedElement] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClickInvoice = (invoice) => {
+    setSelectedElement(invoice);
+    setShowModal(true);
+  };
+
   return (
-    <div className="overflow-x-auto rounded-lg border border-gray-800 max-w-[1200px] mx-auto text-xs bg-gray-900/30">
-      <table className="min-w-[500px] w-full  divide-y divide-gray-800 text-center">
-        <thead className="sticky top-0 z-10 bg-gray-900">
-          <tr className="[&_th]:py-2 [&_th]:text-teal-400 [&_th]:font-medium ">
-            <th>FACTURA</th>
-            <th>CLIENTE</th>
-            <ThWithIcon title="MONTO" icon="mynaui:filter" />
-            <ThWithIcon title="FECHA" icon="mynaui:calendar" />
-            <ThWithIcon title="ESTADO" icon="mynaui:filter" />
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-800 [&_>*]:cursor-pointer [&_>*:hover]:bg-gray-800 [&_>*:active]:bg-gray-800">
-          {data.map((invoice) => (
-            <tr key={invoice.id} className="[&_td]:py-4 text-gray-400">
-              <td className="font-bold !text-white">#-{invoice.id}</td>
-              <td>{invoice.client || "-"}</td>
-              <td>{invoice.total_price.toFixed(2)} $</td>
-              <td>{invoice.date}</td>
-              <td>
-                <span
-                  className={`rounded-lg px-2 py-1 ${
-                    PAYMENT_STATUSES[invoice.status].style
-                  }`}
-                >
-                  {PAYMENT_STATUSES[invoice.status].text}
-                </span>
-              </td>
+    <>
+      <div className="overflow-x-auto rounded-lg border border-gray-800 max-w-[1200px] mx-auto text-xs bg-gray-900/30">
+        <table className="min-w-[500px] w-full  divide-y divide-gray-800 text-center">
+          <thead className="sticky top-0 z-10 bg-gray-900">
+            <tr className="[&_th]:py-2 [&_th]:text-teal-400 [&_th]:font-medium ">
+              <th>FACTURA</th>
+              <th>CLIENTE</th>
+              <ThWithIcon title="MONTO" icon="mynaui:filter" />
+              <ThWithIcon title="FECHA" icon="mynaui:calendar" />
+              <ThWithIcon title="ESTADO" icon="mynaui:filter" />
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody className="divide-y divide-gray-800 [&_>*]:cursor-pointer [&_>*:hover]:bg-gray-800 [&_>*:active]:bg-gray-800">
+            {data.map((invoice) => (
+              <tr
+                key={invoice.id}
+                className="[&_td]:py-4 text-gray-400"
+                onClick={() => handleClickInvoice(invoice)}
+              >
+                <td className="font-bold !text-white">#-{invoice.id}</td>
+                <td>{invoice.client || "-"}</td>
+                <td>{invoice.total_price.toFixed(2)} $</td>
+                <td>{invoice.date}</td>
+                <td>
+                  <span
+                    className={`rounded-lg px-2 py-1 ${
+                      PAYMENT_STATUSES[invoice.status].style
+                    }`}
+                  >
+                    {PAYMENT_STATUSES[invoice.status].text}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title={selectedElement?.id}
+      >
+        <ReactQueryProvider>
+          <InvoiceEdit data={selectedElement} setShowModal={setShowModal} />
+        </ReactQueryProvider>
+      </Modal>
+    </>
   );
 };
 export default InvoicesTable;
